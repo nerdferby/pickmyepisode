dae1b9e2d4979c987f51b37d77f28e3bebfe342c
Scott Cummins (Ferby) <scottcummins3356@gmail.com>
2019-03-24 21:41:00 +0000

    Show navigation styling and markup

--- templates/components/show-nav-present.tpl.php
+++ templates/components/show-nav-present.tpl.php
@@ -1,5 +1,15 @@
 <?php
 
-echo $show->name;
-echo $show->number_of_seasons;
-echo "<img src='".$show->preview_src."' />";
+//echo $show->name;
+//echo $show->number_of_seasons;
+//echo "<img src='".$show->preview_src."' />";
+?>
+
+
+<li class="nav__item nav__item--<?php echo explode("/", $show->page_url)[1] ?>">
+	<a href="<?php echo $show->page_url ?>" >
+		<span class="show-title">
+			<?php echo $show->name ?>
+		</span>
+	</a>
+</li>
--- templates/main.tpl.php
+++ templates/main.tpl.php
@@ -29,18 +29,28 @@
 		<input type="hidden" id="show-id" value="<?php echo $show_id ?>" />
 		<input type="hidden" id="number-of-seasons" value="<?php echo $show->number_of_seasons ?>" />
 
-		<?php
-		// TODO style navigation items
-		include($_SERVER["DOCUMENT_ROOT"]."/includes/show-navigation.inc");
-		render_show_navigation_item(2316, $show_id);
-		render_show_navigation_item(1668, $show_id);
-		render_show_navigation_item(1100, $show_id);
-
-		?>
-
 		<script src="../js/pick-episode-script.src.js"></script>
 
 	</main>
+
+	<nav class="nav">
+
+		<h2>
+			Maybe something else?
+		</h2>
+
+		<ul>
+			<?php
+			// TODO style navigation items
+			include($_SERVER["DOCUMENT_ROOT"]."/includes/show-navigation.inc");
+			render_show_navigation_item(2316, $show_id);
+			render_show_navigation_item(1668, $show_id);
+			render_show_navigation_item(1100, $show_id);
+
+			?>
+		</ul>
+
+	</nav>
 
 	<footer>
 		<?php include("components/footer.tpl.php") ?>
